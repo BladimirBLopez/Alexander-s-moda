5c299e36c31762908595c71760e58f3603614ab5
BladimirBLopez <blopezbladimir@gmail.com>
2026-08-21 21:53:23 -0400

    Agrega fotos de la seccion Femenino/Masculino para Sport

--- lib/productos.ts
+++ lib/productos.ts
@@ -55,8 +55,8 @@ export const productos: Producto[] = [
     fotoMujerFuera: "https://res.cloudinary.com/dkq95jus0/image/upload/v1787361649/Dise%C3%B1o_sin_t%C3%ADtulo_4_ayqhdq.png",
     fotoHombreDentro: "https://res.cloudinary.com/dkq95jus0/image/upload/e_trim/v1786755139/promo-hombre_1.png",
     fotoHombreFuera: "https://res.cloudinary.com/dkq95jus0/image/upload/e_trim/v1786755139/promo-hombre_1.png",
-    fotoMujerSeccion2: "https://res.cloudinary.com/dkq95jus0/image/upload/e_trim/v1786755139/promo-mujer_2.png",
-    fotoHombreSeccion2: "https://res.cloudinary.com/dkq95jus0/image/upload/e_trim/v1786755139/promo-hombre_2.png",
+    fotoMujerSeccion2: "https://res.cloudinary.com/dkq95jus0/image/upload/v1787361943/Dise%C3%B1o_sin_t%C3%ADtulo_2_ywe8v4.png",
+    fotoHombreSeccion2: "https://res.cloudinary.com/dkq95jus0/image/upload/v1787361943/Dise%C3%B1o_sin_t%C3%ADtulo_4_ys77im.png",
   },
   {
     id: "uniforme-casimir",
