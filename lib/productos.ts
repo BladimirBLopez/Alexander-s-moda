f7c4975c5ae5de5003c9369a7b19b6384db72477
BladimirBLopez <blopezbladimir@gmail.com>
2026-09-08 19:37:00 -0400

    Reemplazar imagen de arriba de CASIMIR (sección inferior)

--- lib/productos.ts
+++ lib/productos.ts
@@ -90,7 +90,7 @@ export const productos: Producto[] = [
     fotoMujerFuera: "https://res.cloudinary.com/dkq95jus0/image/upload/v1788893478/20260908_144638_0000_ovkxne.png",
     fotoHombreDentro: "https://res.cloudinary.com/dkq95jus0/image/upload/v1788899900/20260908_163401_0000_xy5dgx.png",
     fotoHombreFuera: "https://res.cloudinary.com/dkq95jus0/image/upload/v1788899900/20260908_163618_0000_z7jbyg.png",
-    fotoSeccion2Arriba: "https://res.cloudinary.com/dkq95jus0/image/upload/v1788903726/20260908_173842_0000_ukxgv5.png",
+    fotoSeccion2Arriba: "https://res.cloudinary.com/dkq95jus0/image/upload/v1788910438/20260908_193254_0000_uvkkoh.png",
     fotoSeccion2Izquierda: "https://res.cloudinary.com/dkq95jus0/image/upload/v1788903743/20260908_173713_0000_pjkpj8.png",
     fotoSeccion2Derecha: "https://res.cloudinary.com/dkq95jus0/image/upload/v1788908701/New_ilzrqt.png",
   },
